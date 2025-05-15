f0a49224c9b2459da20a8c68d2a879276db48eab
Daria <dariaantonia.sandulescu@gmail.com>
2025-05-15 17:41:31 +0300

    template - completare

---
+++ Template/src/ro/cts/classes/TramvaiTemporar.java
@@ -0,0 +1,45 @@
+package ro.cts.classes;
+
+public class TramvaiTemporar extends TramvaiAbstract {
+
+    private int nrLinie;
+
+    public TramvaiTemporar(int nrLinie) {
+        this.nrLinie = nrLinie;
+    }
+
+    @Override
+    protected void opresteInS1() {
+        System.out.println("A ajuns la pipera");
+    }
+
+    @Override
+    protected void opresteInS2() {
+        System.out.println("lucrari... nu oprim");
+
+    }
+
+    @Override
+    protected void opresteInS3() {
+        System.out.println("A ajuns in tei");
+
+    }
+
+    @Override
+    protected void opresteInS4() {
+        System.out.println("A ajuns la lizeanu");
+
+    }
+
+    @Override
+    protected void opresteInS5() {
+        System.out.println("pur si simplu nu oprim...");
+
+    }
+
+    @Override
+    protected void opresteInS6() {
+        System.out.println("A ajuns la sf vineri");
+
+    }
+}
